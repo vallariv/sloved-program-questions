20e99c0370b8a2549277edc5da047d15e1316bed
vallari <124878456+vallariv@users.noreply.github.com>
2023-10-19 22:47:07 +0530

    Create product.py

---
+++ product.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+product_result = num1 * num2
+print(f"The product of {num1} and {num2} is {product_result}")
