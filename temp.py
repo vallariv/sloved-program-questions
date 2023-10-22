9bf71c31948068a620f1775a25086a39098109da
vallari <124878456+vallariv@users.noreply.github.com>
2023-10-22 18:13:51 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,8 @@
+# Input temperature in Celsius
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Convert to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Display the result
+print(f"{celsius}°C is equal to {fahrenheit}°F")
