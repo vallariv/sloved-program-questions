f983323188cb1d8e3e2e9f84022c921ef2f34176
vallari <124878456+vallariv@users.noreply.github.com>
2023-10-22 19:18:26 +0530

    Create 49.py

---
+++ 49.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+matrix1 = np.array([[1, 2], [3, 4]])
+matrix2 = np.array([[1, 2], [3, 4]])
+are_equal = np.array_equal(matrix1, matrix2)
+print("Are the matrices equal?", are_equal)
