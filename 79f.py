f819e43b57869f1745f04abdc2e6945107ba4424
vallari <124878456+vallariv@users.noreply.github.com>
2023-10-22 20:22:30 +0530

    Create 79f.py

---
+++ 79f.py
@@ -0,0 +1,5 @@
+word_list = ["apple", "banana", "cherry", "date", "elderberry"]
+longest_word = max(word_list, key=len)
+length_of_longest_word = len(longest_word)
+print("Longest word in the list:", longest_word)
+print("Length of the longest word:", length_of_longest_word)
